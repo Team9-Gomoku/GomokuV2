c40328cc766bc965406ae39342ea2410d767efe1
NgoPCuong <cuonngo93@gmail.com>
2017-02-04 20:48:47 -0800

    First commit for checking horizontal.

--- Gomoku/app/src/main/java/edu/pdx/cs/cs554/gomoku/GridView.java
+++ Gomoku/app/src/main/java/edu/pdx/cs/cs554/gomoku/GridView.java
@@ -67,6 +67,8 @@ public class GridView extends View {
         invalidate();
     }
 
+    //Put invisible stones on the edge of the board
+    //So black and white cannot be placed on board edge
     private void blockEdgeBoard() {
         for(int column = 0; column < numColumns; column++) {
             //Log.i("INFO", String.valueOf(column) + "," + String.valueOf(0));
@@ -79,40 +81,30 @@ public class GridView extends View {
             cellChecked[numColumns-1][row] = "BLANK";
         }
     }
-    /*
+
+    //=========CHECK WINNER=============
     private boolean findWinner() {
         return checkHorizontal("WHITE");
-        //checkHorizontal("BLACK");
-        ///checkVertical("WHITE");
-        //checkVertical("BLACK");
-        //return checkRightDiagonal("WHITE");
     }
 
-    private boolean isNotBlockedEnd(int column, int row) {
-        Log.d("DEBUG", String.valueOf(column) + ", " + String.valueOf(row));
-        if ((cellChecked[column][row]) == null)
-            return true;
-        Log.d("DEBUG", String.valueOf(column) + ", " + String.valueOf(row) + "\t is blocked");
-        return false;
-    }
-
-
-    //Find Winner by doing horizontal check
+    //Find Winner by doing horizontal check.
+    //Return true if found a winner
     private boolean checkHorizontal(String playerColor) {
         boolean isWinner = false;
         for (int row = 0; row < numRows; row++) {
             int score = 0;
             for (int column = 0; column < numColumns; column++) {
-                //Log.d("DEBUG", String.valueOf(column) + ", " + String.valueOf(row));
                 if (cellChecked[column][row] == playerColor && score < 5) {
                     score++;
-                    //Log.i("INFO", "SCORE:" + score);
+                    Log.i("INFO", "SCORE:" + score);
                     if(score == 5) {
-                        if(isNotBlockedEnd(column, row) || isNotBlockedEnd(column-5, row)) {
+                        if(isNotBlockedEnd(column+1, row, playerColor) ||
+                                isNotBlockedEnd(column-5, row, playerColor)) {
                             Log.i("INFO", playerColor + " IS THE WINNER");
                             isWinner = true;
                             break;
                         } else {
+                            Log.i("INFO", "Blocked at both ends");
                             score = 0;
                             isWinner = false;
                         }
@@ -128,8 +120,107 @@ public class GridView extends View {
         }
         return isWinner;
     }
-    */
+
+    //Check if the end is blocked
+    private boolean isNotBlockedEnd(int column, int row, String playerColor) {
+        if ((cellChecked[column][row]) == null)
+            return true;
+        return false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+
+        if (numColumns == 0 || numRows == 0) {
+            return;
+        }
+
+        int width = getWidth();
+        int height = getHeight();
+
+        //This block will draw the grid based on the number of columns and rows.
+        for (int i = 1; i < numColumns + 1; i++) {
+            canvas.drawLine(i * cellWidth, cellHeight, i * cellWidth, numRows * cellHeight, blackPaint);
+        }
+
+        for (int i = 1; i < numRows + 1; i++) {
+            canvas.drawLine(cellWidth, i * cellHeight, numRows * cellWidth, i * cellHeight, blackPaint);
+
+        }
+
+        //This block will draw the stone on the board
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
+                if (cellChecked[i][j] != null) {
+                    if (cellChecked[i][j] == "WHITE"){
+                        canvas.drawCircle((i+1) *cellWidth, (j+1)*cellHeight, cellWidth/3, greyPaint);
+                    } else if (cellChecked[i][j] == "BLACK") {
+                        canvas.drawCircle((i+1)*cellWidth, (j+1)*cellHeight, cellWidth/3, blackPaint);
+                    }
+
+                    /*TEST
+                    else {
+                        canvas.drawCircle((i+1)*cellWidth, (j+1)*cellHeight, cellWidth/3, blackPaint);
+                    }*/
+                }
+            }
+        }
+
+        findWinner();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            //This block of code will round up the to the board intersection position.
+            float xPosition = (float) event.getX()/cellWidth;
+            float yPosition = (float) event.getY()/cellHeight;
+            int column = (int)(Math.round(xPosition)) -1;
+            int row = (int)(Math.round(yPosition))-1;
+
+            /*DEBUG
+            Log.d("DEBUG", "column: " + xPosition);
+            Log.d("DEBUG", "row: " + yPosition);
+            Log.d("DEBUG", "column: " + column);
+            Log.d("DEBUG", "row: " + row);
+            */
+
+            //If position is out of the grid
+            if (column < 0 || row < 0) {
+                return false;
+            }
+
+            if (column >= numColumns || row >= numRows) {
+                return false;
+            }
+
+            //If position is already placed by other stone
+            if(cellChecked[column][row] != null) {
+                return false;
+            }
+
+            //Alternate the stone color
+            if (activePlayer == 0){
+                cellChecked[column][row] = "WHITE";
+                activePlayer = 1;
+            } else {
+                cellChecked[column][row] = "BLACK";
+                activePlayer = 0;
+            }
+
+
+            Log.i("INFO", cellChecked[column][row] + ": "+ String.valueOf(column) + " , " + String.valueOf(row));
+            invalidate();
+        }
+
+        return true;
+    }
+
+
     /*
+
     private boolean checkVertical(String playerColor) {
         int score = 0;
         boolean isWinner = false;
@@ -182,93 +273,4 @@ public class GridView extends View {
     private boolean checkLeftDiagonal(String playerColor)
     */
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.WHITE);
-
-        if (numColumns == 0 || numRows == 0) {
-            return;
-        }
-
-        int width = getWidth();
-        int height = getHeight();
-
-        //This block will draw the grid based on the number of columns and rows.
-        for (int i = 1; i < numColumns + 1; i++) {
-            canvas.drawLine(i * cellWidth, cellHeight, i * cellWidth, numRows * cellHeight, blackPaint);
-        }
-
-        for (int i = 1; i < numRows + 1; i++) {
-            canvas.drawLine(cellWidth, i * cellHeight, numRows * cellWidth, i * cellHeight, blackPaint);
-
-        }
-
-        //This block will draw the stone on the board
-        for (int i = 0; i < numColumns; i++) {
-            for (int j = 0; j < numRows; j++) {
-                if (cellChecked[i][j] != null) {
-                    if (cellChecked[i][j] == "WHITE"){
-                        canvas.drawCircle((i+1) *cellWidth, (j+1)*cellHeight, cellWidth/3, greyPaint);
-                    } else if (cellChecked[i][j] == "BLACK") {
-                        canvas.drawCircle((i+1)*cellWidth, (j+1)*cellHeight, cellWidth/3, blackPaint);
-                    }
-
-                    /*TEST
-                    else {
-                        canvas.drawCircle((i+1)*cellWidth, (j+1)*cellHeight, cellWidth/3, blackPaint);
-                    }*/
-                }
-            }
-        }
-
-        //findWinner();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-            //This block of code will round up the to the board intersection position.
-            float xPosition = (float) event.getX()/cellWidth;
-            float yPosition = (float) event.getY()/cellHeight;
-            int column = (int)(Math.round(xPosition)) -1;
-            int row = (int)(Math.round(yPosition))-1;
-
-            /*DEBUG
-            Log.d("DEBUG", "column: " + xPosition);
-            Log.d("DEBUG", "row: " + yPosition);
-            Log.d("DEBUG", "column: " + column);
-            Log.d("DEBUG", "row: " + row);
-            */
-
-            //If position is out of the grid
-            if (column < 0 || row < 0) {
-                return false;
-            }
-
-            if (column >= numColumns || row >= numRows) {
-                return false;
-            }
-
-            //If position is already placed by other stone
-            if(cellChecked[column][row] != null) {
-                return false;
-            }
-
-            //Alternate the stone color
-            if (activePlayer == 0){
-                cellChecked[column][row] = "WHITE";
-                activePlayer = 1;
-            } else {
-                cellChecked[column][row] = "BLACK";
-                activePlayer = 0;
-            }
-
-
-            Log.i("INFO", cellChecked[column][row] + ": "+ String.valueOf(column) + " , " + String.valueOf(row));
-            invalidate();
-        }
-
-        return true;
-    }
 }
