6fb63b8b2f087ebdbf59c7f8a0714c97576797a9
NgoPCuong <cuonngo93@gmail.com>
2017-02-24 17:59:26 -0800

    COMPUTER WINNING STEP MOVE

--- Gomoku/app/src/main/java/edu/pdx/cs/cs554/gomoku/Board.java
+++ Gomoku/app/src/main/java/edu/pdx/cs/cs554/gomoku/Board.java
@@ -215,18 +215,6 @@ public class Board extends View {
         return isWinner;
     }
 
-    /*
-        for( int k = 0 ; k < dim * 2 ; k++ ) {
-        for( int j = 0 ; j <= k ; j++ ) {
-            int i = k - j;
-            if( i < dim && j < dim ) {
-                System.out.print( array[i][j] + " " );
-            }
-        }
-        System.out.println();
-    }
-     */
-
     //Check right diagonal ↗↗↗↗↗↗
     private boolean checkRightDiagonal(String playerColor) {
         boolean isWinner = false;
@@ -333,104 +321,6 @@ public class Board extends View {
         }
         return isWinner;
     }
-
-    /*
-    //TODO NEED TO REFACTOR THIS LATER ON
-    private boolean checkLeftDiagonal(String playerColor) {
-        boolean isWinner = false;
-        int score = 0;
-
-        int i,j;
-
-        //Check Bottom half
-        for(i=numRows-1; i>=0; i--){
-            int row = i;
-            int column= 0;
-            score = 0;
-            while(row<numRows){
-                //Log.i("INFO", String.valueOf(column) + "," + String.valueOf(row));
-                if( row < numRows && column < numColumns ) {
-                    if (cellChecked[column][row] == playerColor && score < 5) {
-                        score++;
-                        //Log.i("INFO", "SCORE: " + String.valueOf(score));
-                        if(score == 5){
-                            //CHECK if there's NO 6 in a row AND
-                            // (left ends is NULL OR right ends is NULL)
-                            // checks XOOOOO   or  OOOOOX or OOOOO
-                            if(!isNotBlockedEnd(column+1, row+1, playerColor) &&
-                                    (isNotBlockedEnd(column-5, row-5) || isNotBlockedEnd(column+1, row+1))) {
-                                Log.i("INFO", playerColor + " IS THE WINNER");
-                                isWinner = true;
-                                break;
-                            }
-
-                            // checks OOOOOO (6x) or more
-                            if (mode.equals(GameMode.FREESTYLE) && isNotBlockedEnd(column+1, row+1, playerColor)) {
-                                Log.i("INFO", playerColor + " IS THE WINNER in freestyle mode");
-                                isWinner = true;
-                                break;
-                            }
-                            score = 0;
-                            isWinner = false;
-                        }
-                    }   else {
-                        score = 0;
-                        isWinner = false;
-                    }
-                }
-                column++;
-                row++;
-            }
-            if(isWinner)
-                break;
-        }
-        if(isWinner) {
-            return true;
-        }
-
-
-        //Check the upper half
-        for(i=1; i<numColumns; i++){
-            int column = i;
-            int row = 0;
-            while(column<numColumns){
-                if( row < numRows && column < numColumns ) {
-                    if (cellChecked[column][row] == playerColor && score < 5) {
-                        score++;
-                        if(score == 5){
-                            //CHECK if there's NO 6 in a row AND
-                            // (left ends is NULL OR right ends is NULL)
-                            // checks XOOOOO   or  OOOOOX or OOOOO
-                            if(!isNotBlockedEnd(column+1, row+1, playerColor) &&
-                                    (isNotBlockedEnd(column-5, row-5) || isNotBlockedEnd(column+1, row+1))) {
-                                Log.i("INFO", playerColor + " IS THE WINNER");
-                                isWinner = true;
-                                break;
-                            }
-
-                            // checks OOOOOO (6x) or more
-                            if (mode.equals(GameMode.FREESTYLE) && isNotBlockedEnd(column+1, row+1, playerColor)) {
-                                Log.i("INFO", playerColor + " IS THE WINNER in freestyle mode");
-                                isWinner = true;
-                                break;
-                            }
-                            score = 0;
-                            isWinner = false;
-                        }
-                    }   else {
-                        score = 0;
-                        isWinner = false;
-                    }
-                }
-                column++;
-                row++;
-            }
-            if(isWinner)
-                break;
-        }
-
-        return isWinner;
-    }*/
 
     private void drawBoard(Canvas canvas) {
         if (numColumns == 0 || numRows == 0) {
@@ -544,16 +434,285 @@ public class Board extends View {
 
 
     private void computerMove(int column, int row) {
-        int [] cellToPlace = threatSequences();
+
+        int [] cellToPlace = null;
+
+        cellToPlace = ComputerWinningStep("BLACK");
+        if(cellToPlace != null) {
+            Log.i("INFO", "COMPUTER WINING STEP DETECTED");
+            cellChecked[cellToPlace[0]][cellToPlace[1]] = "BLACK";
+            return;
+        }
+
+        cellToPlace = threatSequences();
 
         if(cellToPlace != null) {
-            Log.i("INFO", "DETECTED THREAT");
+            Log.i("INFO", "THREAT STEP DETECTED");
             cellChecked[cellToPlace[0]][cellToPlace[1]] = "BLACK";
         } else if (cellToPlace == null) {
             cellToPlace = NoThreatMove(column, row);
             cellChecked[cellToPlace[0]][cellToPlace[1]] = "BLACK";
         }
     }
+
+    private int [] ComputerWinningStep(String playerColor) {
+        int [] computerMove;
+
+        computerMove = checkComputerWinningStepHorizontal(playerColor);
+
+        if(computerMove == null) {
+            computerMove = checkComputerWinningStepVertical(playerColor);
+        }
+
+        if(computerMove == null) {
+            computerMove = checkComputerWinningStepRightDiagonal(playerColor);
+        }
+
+        if(computerMove == null) {
+            computerMove = checkComputerWinningStepLeftDiagonal(playerColor);
+        }
+
+        return computerMove;
+    }
+
+
+    private int [] checkComputerWinningStepHorizontal(String playerColor) {
+        for (int row = 0; row < numRows; row++) {
+            int score = 0;
+            for (int column = 0; column < numColumns; column++) {
+
+                if (cellChecked[column][row] == playerColor && score < 4) {
+                    score++;
+
+                    if (score == 2) {
+                        // O O _ O O -> O O O O O
+                        if(isNotBlockedEnd(column - 2, row, null) &&
+                                isNotBlockedEnd(column - 3, row, playerColor) &&
+                                isNotBlockedEnd(column - 4, row, playerColor)) {
+                            return new int [] {column - 2, row};
+                        }
+                    }
+
+                    if (score == 3) {
+                        // O O O _ O -> O O O O O
+                        // O _ O O O -> O O O O O
+                        // O O O _ O -> O O O O O
+                        // O _ O O O -> O O O O O
+                        if (isNotBlockedEnd(column + 1, row, null) &&
+                                isNotBlockedEnd(column + 2, row, playerColor)) {
+                            return new int [] {column + 1, row};
+                        } else if (isNotBlockedEnd(column - 3, row, null) &&
+                                isNotBlockedEnd(column - 4, row, playerColor)) {
+                            return new int [] {column - 3, row};
+                        } else if (isNotBlockedEnd(column + 1, row, null) &&
+                                isNotBlockedEnd(column + 2, row, playerColor)) {
+                            return new int[] {column + 1, row};
+                        }
+                    }
+
+                    if (score == 4) {
+                        // (_|B) O O O O _ -> O O O O O
+                        // _ O O O O B-> O O O O O
+                        if(isNotBlockedEnd(column + 1, row, null)) {
+                            return new int [] {column + 1, row};
+                        } else if (isNotBlockedEnd(column - 4, row, null)) {
+                            return new int [] {column - 4, row};
+                        }
+                    }
+                } else {
+                    score = 0;
+                }
+            }
+        }
+        return null;
+    }
+
+    private int [] checkComputerWinningStepVertical(String playerColor) {
+        for (int column = 0; column < numColumns; column++) {
+            int score = 0;
+            for (int row = 0; row < numRows; row++) {
+
+                if (cellChecked[column][row] == playerColor && score < 4) {
+                    score++;
+
+                    if (score == 2) {
+                        // O O _ O O -> O O O O O
+                        if(isNotBlockedEnd(column, row - 2, null) &&
+                                isNotBlockedEnd(column, row - 3, playerColor) &&
+                                isNotBlockedEnd(column, row - 4, playerColor)) {
+                            return new int [] {column, row - 2};
+                        }
+                    }
+
+                    if (score == 3) {
+                        // O O O _ O -> O O O O O
+                        // O _ O O O -> O O O O O
+                        // O O O _ O -> O O O O O
+                        // O _ O O O -> O O O O O
+                        if (isNotBlockedEnd(column, row + 1, null) &&
+                                isNotBlockedEnd(column, row + 2, playerColor)) {
+                            return new int [] {column, row + 1};
+                        } else if (isNotBlockedEnd(column, row - 3, null) &&
+                                isNotBlockedEnd(column, row - 4, playerColor)) {
+                            return new int [] {column, row - 3};
+                        } else if (isNotBlockedEnd(column + 1, row + 1, null) &&
+                                isNotBlockedEnd(column, row + 2, playerColor)) {
+                            return new int[] {column, row + 1};
+                        }
+                    }
+
+                    if (score == 4) {
+                        // (_|B) O O O O _ -> O O O O O
+                        // _ O O O O B-> O O O O O
+                        if(isNotBlockedEnd(column, row + 1, null)) {
+                            return new int [] {column, row + 1};
+                        } else if (isNotBlockedEnd(column, row - 4, null)) {
+                            return new int [] {column, row - 4};
+                        }
+                    }
+                } else {
+                    score = 0;
+                }
+            }
+        }
+        return null;
+    }
+
+    private int [] checkComputerWinningStepRightDiagonal(String playerColor) {
+        for( int k = 0 ; k < numColumns * 2 ; k++ ) {
+            int score = 0;
+            for( int column = 0 ; column <= k ; column++ ) {
+                int row = k - column;
+                if( row < numColumns && column < numColumns ) {
+                    //cellChecked[column][row] = "BLACK";
+                    if (cellChecked[column][row] == playerColor && score < 5) {
+                        score++;
+
+                        if (score == 2) {
+                            // O O _ O O -> O O X O O
+                            if(isNotBlockedEnd(column - 2, row + 2, null) &&
+                                    isNotBlockedEnd(column - 3, row + 3, playerColor) &&
+                                    isNotBlockedEnd(column - 4, row + 4, playerColor)) {
+                                return new int [] {column - 2, row + 2};
+                            }
+                        }
+
+                        if (score == 3) {
+                            // O O O _ O -> O O O X O
+                            // O _ O O O -> O X O O O
+                            // X O O O _ O
+                            // O _ O O O X
+                            if (isNotBlockedEnd(column + 1, row - 1, null) &&
+                                    isNotBlockedEnd(column + 2, row - 2, playerColor)) {
+                                return new int [] {column + 1, row - 1};
+                            } else if (isNotBlockedEnd(column - 3, row + 3, null) &&
+                                    isNotBlockedEnd(column - 4, row + 4, playerColor)) {
+                                return new int [] {column - 3, row + 3};
+                            } else if (isNotBlockedEnd(column + 1, row - 1, null) &&
+                                    isNotBlockedEnd(column + 2, row - 2, playerColor)) {
+                                return new int[] {column + 1, row - 1};
+                            } else if (isNotBlockedEnd(column - 3, row + 3, null) &&
+                                    isNotBlockedEnd(column - 4, row + 4, playerColor)) {
+                                return new int[] {column - 3, row + 3};
+                            }
+                        }
+
+                        if (score == 4) {
+                            // (_|B|X) O O O O _  ->  (_|B|X) O O O O X
+                            // X O O O O (_|B|X)  ->  O O O O O (_|B|X)
+                            if (isNotBlockedEnd(column + 1, row - 1)) {
+                                return new int[] {column + 1, row - 1};
+                            } else if (isNotBlockedEnd(column - 4, row + 4)) {
+                                return new int[] {column - 4, row + 4};
+                            }
+                        }
+
+                    } else {
+                        score = 0;
+                    }
+                }
+            }
+            score = 0;
+        }
+        return null;
+    }
+
+    private int [] checkComputerWinningStepLeftDiagonal(String playerColor) {
+        int score = 0;
+
+        //number of reverse diagonal
+        int k = numRows + numColumns - 1;
+        int row = numRows - k;
+        for(int i =numRows-1; i>=row; i--){
+            int tmpRow = i;
+            int tmpCol= 0;
+            while(tmpRow<numRows && tmpCol<numColumns){
+                if(tmpRow<0){
+                    tmpCol++;
+                    tmpRow++;
+                    continue;
+                }else{
+                    if (cellChecked[tmpCol][tmpRow] == playerColor && score < 5) {
+                        score++;
+
+
+                        if (score == 2) {
+                            // O O _ O O -> O O X O O
+                            if(isNotBlockedEnd(tmpCol - 2, tmpRow - 2, null) &&
+                                    isNotBlockedEnd(tmpCol - 3, tmpRow - 3, playerColor) &&
+                                    isNotBlockedEnd(tmpCol - 4, tmpRow - 4, playerColor)) {
+                                return new int [] {tmpCol - 2, tmpRow - 2};
+                            }
+                        }
+
+                        if (score == 3) {
+                            // O O O _ O -> O O O X O
+                            // O _ O O O -> O X O O O
+                            // X O O O _ O
+                            // O _ O O O X
+                            if (isNotBlockedEnd(tmpCol + 1, tmpRow + 1, null) &&
+                                    isNotBlockedEnd(tmpCol + 2, tmpRow + 2, playerColor)) {
+                                return new int [] {tmpCol + 1, tmpRow + 1};
+                            } else if (isNotBlockedEnd(tmpCol - 3, tmpRow - 3, null) &&
+                                    isNotBlockedEnd(tmpCol - 4, tmpRow - 4, playerColor)) {
+                                return new int [] {tmpCol - 3, tmpRow - 3};
+                            } else if (isNotBlockedEnd(tmpCol + 1, tmpRow + 1, null) &&
+                                    isNotBlockedEnd(tmpCol + 2, tmpRow + 2, playerColor)) {
+                                return new int[] {tmpCol + 1, tmpRow + 1};
+                            } else if (isNotBlockedEnd(tmpCol - 3, tmpRow - 3, null) &&
+                                    isNotBlockedEnd(tmpCol - 4, tmpRow - 4, playerColor)) {
+                                return new int[] {tmpCol - 3, tmpRow - 3};
+                            }
+                        }
+
+
+                        if (score == 4) {
+                            // (_|B|X) O O O O _  ->  (_|B|X) O O O O X
+                            // X O O O O (_|B|X)  ->  O O O O O (_|B|X)
+                            if (isNotBlockedEnd(tmpCol + 1, tmpRow + 1)) {
+                                return new int[] {tmpCol + 1, tmpRow + 1};
+                            } else if (isNotBlockedEnd(tmpCol - 4, tmpRow - 4)) {
+                                return new int[] {tmpCol - 4, tmpRow - 4};
+                            }
+                        }
+
+                    } else {
+                        score = 0;
+                    }
+                    tmpCol++;
+                    tmpRow++;
+
+                }
+            }
+            score = 0;
+        }
+        return null;
+    }
+
+
+
+
+
 
     private int [] NoThreatMove(int column, int row) {
 
@@ -578,7 +737,8 @@ public class Board extends View {
     }
 
     private int [] threatSequences () {
-        int [] computerMove;
+        int [] computerMove = null;
+        /*
         computerMove = checkThreatHorizontal("WHITE");
 
         if(computerMove == null) {
@@ -591,7 +751,7 @@ public class Board extends View {
 
         if(computerMove == null) {
             computerMove = checkThreatLeftDiagonal("WHITE");
-        }
+        }*/
         return computerMove;
     }
 
